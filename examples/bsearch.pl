bdbc1526c507c06032577b622b85a673cc05ec7c
David Oswald <daoswald@gmail.com>
2013-09-24 13:37:38 -0600

    Updated examples to work with binsearch

--- examples/bsearch.pl
+++ examples/bsearch.pl
@@ -2,13 +2,14 @@
 
 use strict;
 use warnings;
+no warnings 'once';
 
-use List::BinarySearch qw( bsearch_num );
+use List::BinarySearch qw( binsearch );
 
 my @numbers = ( 32767 .. 65535 );
 my $target  = 60000;
 
-my $found_ix = bsearch_num $target, @numbers;
+my $found_ix = binsearch { $a <=> $b } $target, @numbers;
 
 if ( defined $found_ix ) {
     print "Found $target in range (32767..65535) at index $found_ix.\n";
